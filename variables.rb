7b2ad4fdf8c37502e0f544764e3485817605f1a2
Zach Prince <zachary.s.prince@outlook.com>
2022-02-07 22:21:37 -0500

    Annotate lines

--- variables.rb
+++ variables.rb
@@ -1,8 +1,10 @@
 dog_name = "Spike"
 cat_name = "Delgado"
+# This line assigns the string, "Scyllanara" to the variable lizard_name
 lizard_name = "Scyllarana"
 
 dogs = 32
+# this line assigns the integer 12 to the variable cats
 cats = 12
 lizards = 84
 
@@ -11,6 +13,7 @@ cat_food = 234.3
 lizard_food = 1234.2
 
 lizards_full = true
+# this line assigns the boolean true to the variable cats_full
 cats_full = true
 dogs_full = false
 
